09b81744cc7d1437939ab6d925cb5fb562de453c
Philipp von Weitershausen <philipp@weitershausen.de>
2008-07-29 22:40:41 +0000

    license header

--- src/grokcore/formlib/formlib.py
+++ src/grokcore/formlib/formlib.py
@@ -1,4 +1,19 @@
-import types
+##############################################################################
+#
+# Copyright (c) 2006-2008 Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""Custom implementations of formlib helpers
+"""
+
 from zope import interface, event, lifecycleevent
 from zope.interface.interfaces import IInterface
 from zope.formlib import form
