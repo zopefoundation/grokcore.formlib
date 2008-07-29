c73c5417929d74b27727968e8cbf05d3cb187bba
Philipp von Weitershausen <philipp@weitershausen.de>
2008-07-29 22:38:59 +0000

    remove unneeded stuf

--- src/grokcore/formlib/ftests/test_grok_functional.py
+++ src/grokcore/formlib/ftests/test_grok_functional.py
@@ -5,8 +5,8 @@ import os.path
 
 from pkg_resources import resource_listdir
 from zope.testing import doctest, renormalizing
-from zope.app.testing.functional import (HTTPCaller, getRootFolder,
-                                         FunctionalTestSetup, sync, ZCMLLayer)
+from zope.app.testing.functional import (getRootFolder, FunctionalTestSetup,
+                                         ZCMLLayer)
 
 ftesting_zcml = os.path.join(os.path.dirname(grokcore.formlib.__file__),
                              'ftesting.zcml')
@@ -24,25 +24,6 @@ checker = renormalizing.RENormalizing([
     (re.compile(r'httperror_seek_wrapper:', re.M), 'HTTPError:'),
     ])
 
-def http_call(method, path, data=None, **kw):
-    """Function to help make RESTful calls.
-
-    method - HTTP method to use
-    path - testbrowser style path
-    data - (body) data to submit
-    kw - any request parameters
-    """
-    
-    if path.startswith('http://localhost'):
-        path = path[len('http://localhost'):]
-    request_string = '%s %s HTTP/1.1\n' % (method, path)
-    for key, value in kw.items():
-        request_string += '%s: %s\n' % (key, value)
-    if data is not None:
-        request_string += '\r\n'
-        request_string += data
-    return HTTPCaller()(request_string, handle_errors=False)
-
 def suiteFromPackage(name):
     files = resource_listdir(__name__, name)
     suite = unittest.TestSuite()
@@ -56,10 +37,7 @@ def suiteFromPackage(name):
         test = doctest.DocTestSuite(
             dottedname, setUp=setUp, tearDown=tearDown,
             checker=checker,
-            extraglobs=dict(http=HTTPCaller(),
-                            http_call=http_call,
-                            getRootFolder=getRootFolder,
-                            sync=sync),
+            extraglobs=dict(getRootFolder=getRootFolder),
             optionflags=(doctest.ELLIPSIS+
                          doctest.NORMALIZE_WHITESPACE+
                          doctest.REPORT_NDIFF)
