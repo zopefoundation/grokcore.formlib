96b423301084edd746e85098ce73408f6d2a437f
Jan-Wijbrand Kolman <janwijbrand@gmail.com>
2009-12-13 19:57:33 +0000

    Preparing release 1.5

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ tests_require = [
 
 setup(
     name='grokcore.formlib',
-    version = '1.5dev',
+    version = '1.5',
     author='Grok Team',
     author_email='grok-dev@zope.org',
     url='http://grok.zope.org',
