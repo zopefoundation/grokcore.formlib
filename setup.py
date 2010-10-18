db6131d3dfe5ac38bd7fcad027c0fab67aa817ed
Jan-Jaap Driessen <janjaapdriessen+github@gmail.com>
2010-10-18 11:33:05 +0000

    Preparing release 1.6

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ tests_require = [
 
 setup(
     name='grokcore.formlib',
-    version = '1.6dev',
+    version='1.6',
     author='Grok Team',
     author_email='grok-dev@zope.org',
     url='http://grok.zope.org',
