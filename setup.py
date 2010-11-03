128ed3cd0614c104b91797f62b45181f6cdca76c
Jan-Wijbrand Kolman <janwijbrand@gmail.com>
2010-11-03 16:35:26 +0000

    Back to development: 1.9

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ tests_require = [
 
 setup(
     name='grokcore.formlib',
-    version='1.8',
+    version='1.9dev',
     author='Grok Team',
     author_email='grok-dev@zope.org',
     url='http://grok.zope.org',
