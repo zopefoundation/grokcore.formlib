994e2b873aace3e20fd86c3ba25c47a5b35cd5c5
Jan-Wijbrand Kolman <janwijbrand@gmail.com>
2013-11-18 14:39:36 +0100

    Add a few tests demonstrating the CSRF protection intergration in zope.formlib.

--- src/grokcore/formlib/ftests/form/form.py
+++ src/grokcore/formlib/ftests/form/form.py
@@ -25,6 +25,45 @@ grok.DisplayForm renders a display form:
   ...Really big...
   ...
 
+"Protected" forms only validate and process request data for the given
+HTTP verb - usually POST. In addition, CSRF protection can be enabled.
+
+"Submitting" the form over GET parameters should result in an
+MethodNotAllowed error as the form is configured to only allow submits
+over POST requests::
+
+  >>> browser.open(
+  ...    "http://localhost/manfred/@@editprotected?"
+  ...    "form.name=Manfred&form.size=Big&form.actions.apply=Apply")
+  Traceback (most recent call last):
+  ...
+  MethodNotAllowed: <grokcore.formlib.ftests.form.form.Mammoth object at ...>,
+  <zope.publisher.browser.BrowserRequest
+  instance URL=http://localhost/manfred/@@editprotected>
+
+When CSRF protection is enabled, the corresponding hidden form field is
+rendered by the form templates::
+
+  >>> browser.open("http://localhost/manfred/@@editcsrfprotected")
+  >>> print browser.contents
+  <html>...
+  <input type="hidden" name="__csrftoken__" value="..." />...
+
+  >>> browser.cookies['__csrftoken__'] in browser.contents
+  True
+
+Submitting the form with a wrong token or a missing token, will raise an
+InvalidForm error::
+
+  >>> browser.open("http://localhost/manfred/@@editcsrfprotected")
+  >>> browser.getControl(name="form.name").value = "Manfred the Mammoth"
+  >>> browser.getControl(name="form.size").value = "Really big"
+  >>> browser.getControl(name="__csrftoken__").value = "invalid value"
+  >>> browser.getControl("Apply").click()
+  Traceback (most recent call last):
+  ...
+  InvalidCSRFTokenError: Invalid CSRF token
+
 """
 import grokcore.formlib as grok
 from zope import schema
@@ -39,12 +78,18 @@ class Mammoth(grok.testing.Model):
     implements(IMammoth)
     grok.testing.protect_get(grok.Public, 'name', 'size')
     grok.testing.protect_set(grok.Public, 'name', 'size')
-    
-    name = FieldProperty(IMammoth['name'])    
-    size = FieldProperty(IMammoth['size'])    
+
+    name = FieldProperty(IMammoth['name'])
+    size = FieldProperty(IMammoth['size'])
 
 class Edit(grok.EditForm):
     pass
 
 class Display(grok.DisplayForm):
     pass
+
+class EditProtected(grok.EditForm):
+    method = 'POST'  # only allow submits over POST requests.
+
+class EditCsrfProtected(EditProtected):
+    protected = True  # enables CSRF protection.
